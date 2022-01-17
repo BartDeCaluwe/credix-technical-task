b5d1430c3ac2495678ae5446e267c543f61def31
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-01-17 14:57:50 +0100

    Add: ProgressBar component included is an ugly tailwind hack because the safelist regexp for dynamic width percentages doesn't seem to work properly

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,34 @@
+import { useClamp } from '../hooks/useClamp'
+
+interface ProgressBarProps {
+  value: number
+}
+
+export const ProgressBar = ({ value }: ProgressBarProps) => {
+  const clamped = useClamp(value, 0, 100)
+
+  return (
+    // w-[50%]
+    <div>
+      <div className="text-sm mb-1">{clamped}%</div>
+      <div className="relative min-w-[100px]">
+        <div className="bg-gray-300 h-[4px] w-full"></div>
+        <div
+          className={[
+            'absolute top-0 left-0 h-[4px] flex justify-end items-center',
+            clamped === 0 ? 'bg-gray-300' : 'bg-teal-700',
+            `w-[${clamped}%]`,
+          ].join(' ')}
+        >
+          <span
+            className={[
+              'bg-teal-700 flex-shrink-0 inline-block h-2 w-2 rounded-full',
+              clamped === 0 ? '' : '-mr-1 ',
+            ].join(' ')}
+            aria-hidden="true"
+          ></span>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ stories/ProgressBar.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { ProgressBar } from '../components/ProgressBar'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'ProgressBar',
+  component: ProgressBar,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof ProgressBar>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ProgressBar> = (args) => (
+  <ProgressBar {...args} />
+)
+
+export const Zero = Template.bind({})
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Zero.args = {
+  value: 0,
+}
+
+export const Half = Template.bind({})
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Half.args = {
+  value: 50,
+}
+
+export const Full = Template.bind({})
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Full.args = {
+  value: 100,
+}
