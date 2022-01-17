dd5fc1d29036efbe7bed2024c178434420fe4200
Bart De Caluwe <de.caluwe.bart@gmail.com>
2022-01-17 10:11:54 +0100

    Add: safelist for dynamic classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./stories/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './stories/**/*.{js,ts,jsx,tsx}',
   ],
+  options: {
+    safelist: [/^w-/, /^h-/],
+  },
   theme: {
     extend: {},
   },
